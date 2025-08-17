94b4ab452a272a621b42972a7f9c8a12f065a10d
Audiosurfer <vassauder@gmail.com>
2025-08-17 09:30:08 +0200

    added websecurity with username/password and internal header token for TICKET module.

--- src/main/java/com/simple/partner/security/WebSecurityConfig.java
+++ src/main/java/com/simple/partner/security/WebSecurityConfig.java
@@ -1,22 +1,76 @@
 package com.simple.partner.security;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 
 @Configuration
 @EnableWebSecurity
+@PropertySource("classpath:application.properties")
 public class WebSecurityConfig {
 
+    private static String INTERNAL_TOKEN;
+
+    public WebSecurityConfig(@Value("${internal.token:simple}") String internalToken) {
+        INTERNAL_TOKEN = internalToken;
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Loaded internal token for TICKET module: " + INTERNAL_TOKEN);
+    }
+
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // POST, PUT, DELETE teszteléshez
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // minden request engedélyezve
-                );
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("simple")
+                .password("simple")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    static class InternalServiceAuthFilter extends AbstractPreAuthenticatedProcessingFilter {
+        @Override
+        protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
+            String token = request.getHeader("X-Internal-Token");
+            if (INTERNAL_TOKEN.equals(token)) {
+                return "internal-service";
+            }
+            return null;
+        }
+
+        @Override
+        protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
+            return "N/A";
+        }
     }
 }
